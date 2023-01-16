5d53c75396771b3185647a2254a5d5a9a3e54eb4
valerij79 <121170646+valerij79@users.noreply.github.com>
2023-01-16 13:39:54 +0200

    numbers

---
+++ js/02-numbers.js
@@ -0,0 +1,129 @@
+console.clear();
+/*
+KINTAMUJU INICIAVIMO BUDAI:
+- const (default)
+- let (jei reikes keisti/atnaujinti reiksme)
+- var (niekada nenaudoti)
+NUMBERS:
+- teigiami/neigiami
+- sveikieji/desimtainiai
+- normalus/nenormalus skaiciai:
+    - begalybe
+    - NaN (not-a-number)
+ARITMETINIAI OPERATORIAI: +, -, /, *, ** (laipsnis), % (liekana)
+PRISKIRIMO OPERATORIAI: =, +=, -=, /=, *=, **=, %=
+*/
+
+const metai = 2023;
+const PI = 3.1415;
+
+console.log(metai);
+console.log(PI);
+console.log(PI, metai, PI, metai);
+
+const neigiamas = -6.7;
+
+console.log(neigiamas);
+
+console.log(Infinity);
+console.log(-Infinity);
+console.log(NaN);
+
+const a = 7;
+const b = 5;
+
+const suma = a + b;
+console.log(suma);
+
+const skirtumas = a - b;
+console.log(skirtumas);
+
+const dalmuo = a / b;
+console.log(dalmuo);
+
+const sandauga = a * b;
+console.log(sandauga);
+
+const dvejetai = 2 + 2 * 2;
+console.log(dvejetai);
+
+const dvejetaiSkliaustuose = (2 + 2) * 2;
+console.log(dvejetaiSkliaustuose);
+
+const kampas = 90 * 90;
+const kampas2 = 90 ** 2;
+const kampas3 = 90 ** 3;
+const kampas3_2 = 90 * 90 * 90;
+console.log(kampas, kampas2, kampas3, kampas3_2);
+
+const liekana1 = a % b;
+console.log(liekana1);
+
+const liekana2 = 21 % 3;
+console.log(liekana2);
+
+const liekana3 = 31 % 3;
+console.log(liekana3);
+
+const liekana4 = -41 % 4;
+console.log(liekana4);
+
+const liekana5 = -51 % -5;
+console.log(liekana5);
+
+const liekana6 = 3.1415 % 3;
+console.log(liekana6);
+
+console.log(0.1 + 0.2);
+
+console.log((1 + 2) / 10);
+
+const f = 0.1;
+const g = 0.2;
+const h = (f * 10 + g * 10) / 10;
+console.log(h);
+
+
+console.log('-------------------');
+// console.clear();
+
+let pinigine = 0;
+console.log(pinigine);
+
+pinigine = pinigine + 1;
+console.log(pinigine);
+
+pinigine = pinigine + 2;
+console.log(pinigine);
+
+pinigine = pinigine - 3;
+console.log(pinigine);
+
+pinigine = pinigine + 4;
+console.log(pinigine);
+
+console.log('-------------------');
+
+let temperatura = 0;
+console.log(temperatura);
+
+temperatura = temperatura + 2;
+console.log(temperatura);
+
+temperatura += 3;
+console.log(temperatura);
+
+temperatura -= 1;
+console.log(temperatura);
+
+temperatura *= 2;
+console.log(temperatura);
+
+temperatura /= 2;
+console.log(temperatura);
+
+temperatura **= 2;
+console.log(temperatura);
+
+temperatura %= 3;
+console.log(temperatura);
---
+++ js/03-strings.js
@@ -0,0 +1,82 @@
+console.clear();
+/*
+STRINGS (tekstas)
+Iniciavimo tipai: ' (vienguba), " (dviguba), ` (backtick)
+*/
+
+const vardas = 'Petras';
+console.log(vardas);
+
+const pavarde = "Petraitis";
+console.log(pavarde);
+
+// Petras Petraitis
+const pilnasVardas = vardas + ' ' + pavarde;
+console.log(pilnasVardas);
+
+// Sveiki, as Petras!
+const sveiki = 'Sveiki, as ' + vardas + '!';
+console.log(sveiki);
+
+// Vienguba (') kabute.
+const kabute1 = "Vienguba (') kabute.";
+console.log(kabute1);
+
+// Dviguba (") kabute.
+const kabute2 = 'Dviguba (") kabute.';
+
+// Vienguba (') ir dviguba (") kabutes.
+const priekis = "Vienguba (') ir ";
+const galas = 'dviguba (") kabutes.';
+const kabute12_0 = priekis + galas;
+console.log(priekis);
+console.log(galas);
+console.log(kabute12_0);
+
+const kabute12_1 = 'Vienguba (\') ir dviguba (") kabutes.';
+console.log(kabute12_1);
+
+const kabute12_2 = "Vienguba (') ir dviguba (\") kabutes.";
+console.log(kabute12_2);
+
+const kabute12_3 = 'Vienguba (\') ir dviguba (\") kabutes.';
+console.log(kabute12_3);
+
+const kabute12_4 = "Vienguba (\') ir dviguba (\") kabutes.";
+console.log(kabute12_4);
+
+const spec = '\\as\d\*fdf\\';
+console.log(spec);
+
+console.log('--------------');
+
+/*
+<header>
+    <img src="#">
+    <nav></nav>
+</header>
+*/
+
+const HTML = "<header>\r\n\
+    <img src=\"#\">\r\n\
+    <nav></nav>\r\n\
+</header>";
+console.log(HTML);
+
+const HTML2 = `<header>
+    <img src="#">
+    <nav>backtick (\`)</nav>
+</header>`;
+console.log(HTML2);
+
+const name = 'Ona';
+const age = 77;
+const city = 'Kaimelis';
+
+// Sveiki, mano vardas [vardas], man yra [amzius] metai ir as esu is [vietovardis].
+
+const sms = 'Sveiki, mano vardas ' + name + ', man yra ' + age + ' metai ir as esu is ' + city + '.';
+console.log(sms);
+
+const sms2 = `Sveiki, mano vardas ${name}, man yra ${age} metai ir as esu is ${city}.`;
+console.log(sms2);

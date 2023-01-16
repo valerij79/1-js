396d35fbf632602630bece69c8b1b45eb08db8ae
valerij79 <121170646+valerij79@users.noreply.github.com>
2023-01-16 13:12:23 +0200

    Console

---
+++ js/01-console.js
@@ -0,0 +1,23 @@
+console.clear();
+
+// single-line comment
+
+/*
+multi
+line
+comment
+*/
+
+console.log(5);
+console.log(3, 14);
+console.log(3.14);
+console.log(1, 2, 3, 4, 5, 6);
+
+/*
+Esmines busimos temos:
+- kintamieji (atsiminti informacija, duomenys)
+- palyginimas
+- ciklai (proceduru kartojimas)
+- funkcijos
+- event listeners (narsykleje)
+*/

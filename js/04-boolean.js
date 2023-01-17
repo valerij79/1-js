27b3ceb302814b9548d469a14741416261eb5d44
valerij79 <121170646+valerij79@users.noreply.github.com>
2023-01-17 13:06:19 +0200

    Boolean

---
+++ js/04-boolean.js
@@ -0,0 +1,14 @@
+/*
+BOOLEAN (logine reiksme)
+- true
+- false
+*/
+
+const tiesa = true;
+const melas = false;
+
+console.log(tiesa);
+console.log(melas);
+
+const arPilnametis = true;
+const arLyja = false;
